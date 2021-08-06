69497b6f9fd47c24ffe7716d8066f6fa8a62de6f
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-06 22:00:00 +0800

    changes to css spreadsheet

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -4,7 +4,7 @@ function ExpenseItem() {
   return (
     <div className="expense-item">
       <div>March 28th 2021</div>
-      <div className='express-item__description'>
+      <div className="express-item__description">
         <h2>Car Insurance</h2>
         <div className="expense-item__price">$294.67</div>
       </div>
