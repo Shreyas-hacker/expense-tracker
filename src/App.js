ba96467df7e0ee4793c618ff86307c4c5b5e4201
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-06 10:29:22 +0800

    ExpenseItem component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/Expenseitem'
+
 function App(){
   return (
     <div>
       <h2>Lets get started!</h2>
+      <p>This is also visible</p>
+      <ExpenseItem />
     </div>
   );
 }
