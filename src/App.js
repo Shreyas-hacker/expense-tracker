822793e13578fccec4844685f9cee59999e70010
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-08 00:17:15 +0800

    added props functionality to Expenseitem and array of expenses in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import ExpenseItem from './components/Expenseitem'
+import ExpenseItem from "./components/Expenseitem";
 
-function App(){
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+  ];
   return (
     <div>
       <h2>Lets get started!</h2>
       <p>This is also visible</p>
-      <ExpenseItem />
+      {expenses.map()}
     </div>
   );
 }
