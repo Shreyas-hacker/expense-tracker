99347a3bd0316a4c85c4b45b1537b59b54b0e03b
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-08 01:57:07 +0800

    changed to isostring function

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       id: "e1",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 12).toISOString(),
+      date: new Date(2021, 2, 12),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2021, 2, 12).toISOString(),
+      date: new Date(2021, 2, 12),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28).toISOString(),
+      date: new Date(2021, 2, 28),
     },
   ];
   return (
