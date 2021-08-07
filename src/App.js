92baaa745d4219c49681b88893176241b043ed0b
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-08 01:51:58 +0800

    used maps function to iterate over expenses array

--- src/App.js
+++ src/App.js
@@ -6,26 +6,33 @@ function App() {
       id: "e1",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 12),
+      date: new Date(2021, 2, 12).toISOString(),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2021, 2, 12),
+      date: new Date(2021, 2, 12).toISOString(),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28).toISOString(),
     },
   ];
   return (
     <div>
       <h2>Lets get started!</h2>
       <p>This is also visible</p>
-      {expenses.map()}
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          amount={expense.amount}
+          title={expense.title}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -5,8 +5,8 @@ function ExpenseItem(props) {
     <div className="expense-item">
       <div>{props.date}</div>
       <div className="express-item__description">
-        <h2>{props.expenseTitle}</h2>
-        <div className="expense-item__price">{props.expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
