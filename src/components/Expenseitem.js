6ad183a3c698b4c11e1063b2095a18dc7182ecfe
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-08-12 19:55:53 +0800

    Created ExpenseDate component and css

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,17 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('default', {month:'long'})
-  const day = props.date.toLocaleString('en-US', {day:'2-digit'})
-  const year = props.date.getFullYear()
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="express-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
