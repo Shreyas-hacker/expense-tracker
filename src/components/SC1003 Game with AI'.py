586b9cb0b736c956abbd18a1108b5b83309b3546
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-09-18 15:33:31 +0800

    printing board

--- src/components/SC1003 Game with AI'.py
+++ src/components/SC1003 Game with AI'.py
@@ -1,16 +1,28 @@
 # tic tac toe
 
+board = [' ' for x in range(10)]
+
 def insertLetter(letter,pos):
-    pass
+    board[pos] = letter
 
 def spaceisFree(pos):
-    pass
+    return board[pos] == " "
 
 def printBoard(board):
-    pass
+    print('    |    |')
+    print(' '  +  board[1] + ' | '+board[2]+' | '+board[3])
+    print('    |    |')
+    print('-----------')
+    print('    |    |')
+    print(' '  +  board[4] + ' | '+board[5]+' | '+board[6])
+    print('    |    |')
+    print('-----------')
+    print('    |    |')
+    print(' '  +  board[7] + ' | '+board[8]+' | '+board[9])
+    print('    |    |')
 
 def isWinner(bo,le):
-    pass
+    
 
 def playerMove():
     pass
