43770f4d87f5a1e3e9bc9e1992d459be76c31c17
Shreyas-Hackerr <shreyashegde1@gmail.com>
2021-09-18 15:26:13 +0800

    Functions written out

---
+++ src/components/SC1003 Game with AI'.py
@@ -0,0 +1,31 @@
+# tic tac toe
+
+def insertLetter(letter,pos):
+    pass
+
+def spaceisFree(pos):
+    pass
+
+def printBoard(board):
+    pass
+
+def isWinner(bo,le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove():
+    pass
+
+def selectRandom(board):
+    pass
+
+def isBoardFull(board):
+    pass
+
+def game():
+    pass
+
+if __name__=="__main__":
+    game()
